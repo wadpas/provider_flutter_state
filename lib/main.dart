4b4cc0e9b8eea70f9a4ec87a314e00fa5d189519
wadpas <wadpas@gmail.com>
2023-09-11 14:30:16 +0300

    implemented ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'dart:collection';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      title: 'Flutter Provider',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.red,
+    ChangeNotifierProvider(
+      create: (_) => BreadCrumbProvider(),
+      child: MaterialApp(
+        title: 'Flutter Provider',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.tealAccent,
+          ),
         ),
+        home: const HomePage(),
+        routes: {'/newBreadCrumb': (context) => const Material()},
       ),
-      home: const HomePage(),
     ),
   );
 }
@@ -87,8 +92,25 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: const Center(
-        child: Text('Home Page'),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/newBreadCrumb');
+              },
+              child: const Text('New Breadcrumb'),
+            ),
+            TextButton(
+              onPressed: () {
+                context.read<BreadCrumbProvider>().reset();
+              },
+              child: const Text('Reset'),
+            ),
+          ],
+        ),
       ),
     );
   }
